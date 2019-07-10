0d9d7d5960f4d5ddb9ccd2abb9943a8e7882d2bc
github username <github email address>
2019-07-10 11:38:20 -0400

    search works

--- app/search.rb
+++ app/search.rb
@@ -8,13 +8,24 @@ def search
   search_string = gets.chomp
   if search_string.downcase == "menu"
     menu
-  elsif
-    lesson_subset = Lesson.all.select {|lesson| lesson.class == Lesson}
-      # TODO
-      # search_string in topic
-      # search_string in lesson name
-      # search_string in repo name
-      # search_string in notes
+  elsif     
+    like_search_string = "%#{search_string.downcase}%"  
+    # search_string in topic
+    topic_set = Topic.where("name LIKE ?", like_search_string)
+    topic_ids = topic_set.map {|t| t.id}
+    topic_search = Lesson.all.select {|l| 
+      topic_ids.any?{|tid|
+        tid == l.topic_id
+      }
+    }
+    # search_string in lesson name
+    lesson_search = Lesson.where("title LIKE ?", like_search_string)
+    # search_string in repo name
+    repo_search = Lesson.where("repo LIKE ?", like_search_string)
+    # search_string in notes
+    note_search = Lesson.where("url LIKE ?", like_search_string)
+    # merge search arrays of lessons
+    lesson_subset = topic_search | lesson_search | repo_search | note_search
     pretty_print = lesson_subset.map {|lesson|
       "#{lesson.topic.name} :: #{lesson.title} :: #{lesson.repo} ::
       #{lesson.note_collection}
