b50dde87d3951fc5e2415c77ecf2166bf8661aaa
github username <github email address>
2019-07-11 12:42:00 -0400

    update

--- app/menu.rb
+++ app/menu.rb
@@ -2,7 +2,6 @@ require_relative '../config/environment'
 
 def menu
   puts Rainbow("\nWhat would you like to do? Your options are:\n").cyan
-  # puts "topics          - search and display topics in Learn.co."
   puts Rainbow("    s: search your notes, lessons, repos, and topics
     n: create a note about one of your lessons
     l: open the Learn.co page for a lesson
